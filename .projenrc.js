3727eba4039da04822f98c0f8a4859ce9de75768
Spacepawn <spacepawn@github.com>
2023-03-31 09:49:36 +0200

    Added dev upgrade options and more config

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,23 @@
 const { awscdk } = require("projen");
+const { Stability } = require('projen/lib/cdk');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: "2.1.0",
+  authorName: "Spacepawn",
+  cdkVersion: "2.71.0",
   defaultReleaseBranch: "main",
+  depsUpgradeOptions: {
+    workflowOptions: {
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  description: "Instagrams clone build with AWS CDK, AWS Lambda, and AWS DynamoDB",
+  license: "MIT",
   maxNodeVersion: "18.99.99",
-  minNodeVersion: "18.0.0",
+  minNodeVersion: "18.15.0",
   name: "instagram-lambda",
+  repositoryUrl: "https://github.com/Spacepawn/instagram-lambda.git",
+  //stability: Stability.STABLE,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
